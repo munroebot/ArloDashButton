f70b6681a3ff728fa098306d9eafeb9ec2a5e79e
Brian Munroe <brian.e.munroe@gmail.com>
2018-09-28 11:34:05 -0700

    updating the code

--- ArloDashButton.py
+++ ArloDashButton.py
@@ -1,20 +1,26 @@
+from arlo.Arlo import Arlo
 import os
 import boto3
 
-from pyarlo import PyArlo
-
 def lambda_handler(event, context):
 
-    USERNAME = os.environ['USERNAME']
-    PASSWORD = os.environ['PASSWORD']
+    USERNAME = os.environ['username']
+    PASSWORD = os.environ['password']
     PRIMARY_CONTACT = os.environ['PRIMARY_CONTACT']
     SECONDARY_CONTACT = os.environ['SECONDARY_CONTACT']
+   
+    message = None
 
-    arlo = PyArlo(USERNAME, PASSWORD)
+    arlo = Arlo(USERNAME, PASSWORD)
+    basestation = [ device for device in arlo.GetDevices() if device['deviceType'] == 'basestation' ]
 
     if event['clickType'] == "DOUBLE":
-        base.mode = 'disarmed'
+	arlo.Disarm(basestation[0]['deviceId'], basestation[0]['xCloudId'])
+        message = "Arlo Cameras DISABLED"
     else:
-        base.mode = 'armed'
+        arlo.Arm(basestation[0]['deviceId'], basestation[0]['xCloudId'])
+        message = "Arlo Cameras ENABLED"
 
-   batt_levels = base.get_camera_battery_level
+    sns_client = boto3.client('sns')
+    sns_client.publish(PhoneNumber=PRIMARY_CONTACT,Message=message)
+    sns_client.publish(PhoneNumber=SECONDARY_CONTACT,Message=message)
